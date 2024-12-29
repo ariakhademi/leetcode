459a394edb461264694db86985bf5627a4189111
ariakhademi <ariakhademi@gmail.com>
2024-12-29 17:34:33 -0500

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+"""
+Implementation of selection sort
+"""
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1,len(arr)):
+            if arr[j] < arr[i]:
+                min_idx = j
+                arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
