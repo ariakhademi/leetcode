9a51e083a3678a145495359befb3f63fc2f2d997
ariakhademi <ariakhademi@gmail.com>
2025-01-14 22:34:03 -0500

    permutations

--- permutations.py
+++ permutations.py
@@ -3,3 +3,29 @@ Given an array nums of distinct integers, return all the possible
 permutations
 . You can return the answer in any order.
 """
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        
+        result = []
+        def backtrack(subset):
+            # base case
+            if len(subset) == len(nums):
+                result.append(copy.copy(subset))
+                return
+            # recursive case
+            else:
+                for num in nums:
+                    if num in subset:
+                        continue
+                    subset.append(num)
+                    backtrack(subset)
+                    subset.pop()
+            
+        backtrack([])
+        return result
