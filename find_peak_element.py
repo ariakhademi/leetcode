d0b96c8cb89f6c93003dfa3099792d793f94b2b8
ariakhademi <ariakhademi@gmail.com>
2025-01-15 18:08:17 -0500

    find peak element

---
+++ find_peak_element.py
@@ -0,0 +1,28 @@
+"""
+A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time.
+"""
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            middle = (left + right) // 2
+
+            if nums[middle] > nums[middle + 1]: # there exists a peak on the left side
+                right = middle
+            else:
+                left = middle + 1
+        # either right or left
+        return left
+        
